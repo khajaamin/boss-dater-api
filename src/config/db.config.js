a7e7a1706f00da4c45e8b7f124378fb901ee46db
Talhakhan326 <muhammadtalhakhan326@gmail.com>
2022-09-29 16:02:50 +0500

    fixed

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,12 +1,15 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const {DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME} = process.env;
+const {DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME, DATABASE_URL} = process.env;
 
-const sequelize = new Sequelize(DB_NAME,DB_USER,DB_PASSWORD,{
-    dialect: 'postgres',
-    host:DB_HOST,
-    port:DB_PORT
-})
+const sequelize = new Sequelize(
+DATABASE_URL
+//     DB_NAME, DB_USER, DB_PASSWORD, {
+//     dialect: 'postgres',
+//     host:DB_HOST,
+//     port:DB_PORT
+// }
+)
 
 module.exports = sequelize;
